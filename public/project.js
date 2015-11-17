b98f4c342389afb0f1a4b1bd9f042c49d79f8ed8
Owen Lamb <owen.lamb@gmail.com>
2015-11-17 11:39:17 -0800

    added hero image slider

--- public/project.js
+++ public/project.js
@@ -12,3 +12,20 @@ $( document ).ready(function() {
      }
   });
 });
+
+var imagecount = 1;
+var total = 3;
+function slide(x) {
+	var Image = document.getElementById("img");
+	imagecount = imagecount + x;
+	if(imagecount > total){imagecount = 1;}
+if(imagecount < 1){imagecount = total;}
+Image.src= "Images/img" + imagecount+ ".jpg";
+}
+window.setInterval(function slideA() {
+	var Image = document.getElementById('img');
+	imagecount = imagecount + 1;
+	if(imagecount >  total){imagecount = 1;}
+	if(imagecount < 1){imagecount = total;}
+	Image.src= "Images/img" + imagecount+ ".jpg";
+},10000);
